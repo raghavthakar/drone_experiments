699a0dfb6d03b5d18b64011836c4799926967904
Raghav <raghavthakar12@gmail.com>
2021-09-09 01:33:09 +0530

    rosservice for getting COM

--- src/carriers/src/centre_of_mass_server.cpp
+++ src/carriers/src/centre_of_mass_server.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "sensor_msgs/NavSatFix.h"
 #include "carriers/CentreOfMass.h"
 #include <iostream>
 #include <string>
@@ -7,6 +8,33 @@
 bool get_centre_of_mass(carriers::CentreOfMass::Request &request,
                         carriers::CentreOfMass::Response &response)
 {
+    //Handles the parameter for number of drones
+    int drone_count;
+    ros::param::get("/drone_count", drone_count);
+
+    ROS_INFO("NO of drones: %d", drone_count);
+
+    std::string uav_globalposition_topic;
+    sensor_msgs::NavSatFixConstPtr uav_globalposition;
+    for(int i=0; i<drone_count; i++)
+    {
+        uav_globalposition_topic="/uav"+std::to_string(i)+"/mavros/global_position/global";
+        ROS_INFO("%s", uav_globalposition_topic.c_str());
+        uav_globalposition=ros::topic::waitForMessage<sensor_msgs::NavSatFix>
+                                                        (uav_globalposition_topic);
+
+        ROS_INFO("Drone %d lat %f and long %f", drone_count,
+                uav_globalposition->latitude,uav_globalposition->longitude);
+
+        response.COM.latitude+=uav_globalposition->latitude;
+        response.COM.longitude+=uav_globalposition->longitude;
+        response.COM.altitude+=uav_globalposition->altitude;
+    }
+
+    response.COM.latitude/=drone_count;
+    response.COM.longitude/=drone_count;
+    response.COM.altitude/=drone_count;
+
     ROS_INFO("IN THE SERVICE");
     return true;
 }
