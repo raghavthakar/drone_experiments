97431c235c821985a4b9a29cb7e95622c8aa1703
Raghav Thakar <raghavthakar12@gmail.com>
2022-05-26 15:21:25 -0400

    Token ring without initator

--- src/carriers/src/swarm_member_dev.cpp
+++ src/carriers/src/swarm_member_dev.cpp
@@ -8,9 +8,11 @@
 #include <mavros_msgs/State.h>
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
+#include <std_msgs/String.h>
 
 #define TAKEOFF_HEIGHT 2
 #define FORMATION_RADIUS 2
+#define POSITION_ERROR_LIMIT 0.4
 #define NUM_AGENTS 5
 #define PI 3.1415926535897
 
@@ -22,21 +24,21 @@ void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
-nav_msgs::Odometry global_pos;
-void global_pos_cb(const nav_msgs::Odometry::ConstPtr& msg){
-    global_pos = *msg;
-}
-
 geometry_msgs::PoseStamped local_pos;
 void local_pos_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
     local_pos = *msg;
 }
 
+std_msgs::String token_message;
+void update_newest_token(const std_msgs::String::ConstPtr& msg){
+    token_message = *msg;
+}
+
 // generatee the formation point of agent from centre of formation
-geometry_msgs::PoseStamped waypointToFormationPose(geometry_msgs::PoseStamped global_waypoint, int drone_number)
+geometry_msgs::PoseStamped waypointToFormationPose(geometry_msgs::PoseStamped global_waypoint, int drone_id)
 {
     float d_theta = (float)(PI/NUM_AGENTS);
-    float theta = 2 * d_theta * (drone_number); // anticlockwise measured from positive x axis
+    float theta = 2 * d_theta * (drone_id); // anticlockwise measured from positive x axis
 
     geometry_msgs::PoseStamped formation_point;
 
@@ -44,7 +46,7 @@ geometry_msgs::PoseStamped waypointToFormationPose(geometry_msgs::PoseStamped gl
     formation_point.pose.position.x += FORMATION_RADIUS*std::cos(theta);
     formation_point.pose.position.y += FORMATION_RADIUS*std::sin(theta);
 
-    ROS_INFO("Drone %d global target: %f, %f", drone_number, formation_point.pose.position.x, formation_point.pose.position.y);
+    ROS_INFO("Drone %d global target: %f, %f", drone_id, formation_point.pose.position.x, formation_point.pose.position.y);
     return formation_point;
 }
 
@@ -57,9 +59,44 @@ geometry_msgs::PoseStamped globalToLocalFrame(geometry_msgs::PoseStamped formati
     formation_point_local.pose.position.y = formation_point_global.pose.position.y - origin.pose.position.y;
     formation_point_local.pose.position.z = formation_point_global.pose.position.z - origin.pose.position.z;
 
-    ROS_INFO("Drone global position: %f %f", global_pos.pose.pose.position.x, global_pos.pose.pose.position.y);
     ROS_INFO("Drone local target: %f, %f", formation_point_local.pose.position.x, formation_point_local.pose.position.y);
     return formation_point_local;
+}
+
+bool inPosition(geometry_msgs::PoseStamped formation_point_local)
+{
+    return sqrt((formation_point_local.pose.position.x-local_pos.pose.position.x)*
+                (formation_point_local.pose.position.x-local_pos.pose.position.x)+
+                (formation_point_local.pose.position.y-local_pos.pose.position.y)*
+                (formation_point_local.pose.position.y-local_pos.pose.position.y)+
+                (formation_point_local.pose.position.z-local_pos.pose.position.z)*
+                (formation_point_local.pose.position.z-local_pos.pose.position.z)) < POSITION_ERROR_LIMIT;
+}
+
+void continueTokenRing(ros::Publisher local_pos_pub, geometry_msgs::PoseStamped formation_pose_local, int drone_id, ros::Rate rate,
+                        ros::Publisher token_ring_pub)
+{
+    ROS_INFO("Drone %d is in token ring", drone_id);
+    // maintain old position till the token message is not the current drone_id-1
+    while(ros::ok())
+    {
+        local_pos_pub.publish(formation_pose_local);
+
+        if(token_message.data.compare("TOKEN-"+std::to_string((drone_id+NUM_AGENTS-1)%NUM_AGENTS))==0)
+        {
+            ROS_INFO("Drone %d received token", drone_id);
+            ROS_INFO_STREAM(token_message.data);
+            break;
+        }
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    std_msgs::String token;
+    token.data = "TOKEN-"+std::to_string((drone_id+NUM_AGENTS)%NUM_AGENTS);
+
+    token_ring_pub.publish(token);
 }
 
 int main(int argc, char **argv)
@@ -69,10 +106,12 @@ int main(int argc, char **argv)
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
             ("mavros/state", 10, state_cb);
-    ros::Subscriber global_pos_sub = nh.subscribe<nav_msgs::Odometry>
-            ("mavros/global_position/local", 10, global_pos_cb);
     ros::Subscriber local_pos_sub = nh.subscribe<geometry_msgs::PoseStamped>
             ("mavros/local_position/pose", 10, local_pos_cb);
+    ros::Subscriber token_ring_sub = nh.subscribe<std_msgs::String>
+            ("/token_ring", 10, update_newest_token);
+    ros::Publisher token_ring_pub = nh.advertise<std_msgs::String>
+            ("/token_ring", 10, true);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
@@ -81,17 +120,12 @@ int main(int argc, char **argv)
             ("mavros/set_mode");
     
     // get the process ID from command line
-    int drone_number = std::stoi(argv[1]);
+    int drone_id = std::stoi(argv[1]);
 
-    // get the origin of the agent and set it into local variable
-    std::string origin_x, origin_y, origin_z;
+    // get the origin of the agent and set it into global variable
     nh.getParam("origin/x", origin.pose.position.x);
     nh.getParam("origin/y", origin.pose.position.y);
     nh.getParam("origin/z", origin.pose.position.z);
-    
-    // origin.pose.position.x = stoi(origin_x);
-    // origin.pose.position.y = stoi(origin_y);
-    // origin.pose.position.z = stoi(origin_z);
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
@@ -150,27 +184,53 @@ int main(int argc, char **argv)
         rate.sleep();
     }
 
-    pose.pose.position.y = 7;
-    pose.pose.position.x = 7;
+    pose.pose.position.y = 2;
+    pose.pose.position.x = 2;
 
     // stores the list of waypoints to traverse
     nav_msgs::Path waypoints;
     waypoints.poses.push_back(pose);
 
-    // iterate through target points. Single point for now
+    pose.pose.position.y = 4;
+    pose.pose.position.x = 9;
+    waypoints.poses.push_back(pose);
+
+    pose.pose.position.y = 4;
+    pose.pose.position.x = 14;
+    waypoints.poses.push_back(pose);
+
+    pose.pose.position.y = 4;
+    pose.pose.position.x = 14;
+    pose.pose.position.z = 5;
+    waypoints.poses.push_back(pose);
+
+    pose.pose.position.y = 4;
+    pose.pose.position.x = 4;
+    pose.pose.position.z = 1;
+    waypoints.poses.push_back(pose);
+
+    int waypoint_num = 0;
+    // iterate through target points.
     for(auto global_waypoint:waypoints.poses)
-    {
+    {  
         // convert the target point into formation point for the drone
-        geometry_msgs::PoseStamped formation_pose_global = waypointToFormationPose(global_waypoint, drone_number);
+        geometry_msgs::PoseStamped formation_pose_global = waypointToFormationPose(global_waypoint, drone_id);
         // convert the formation point from global frame to local frame
         geometry_msgs::PoseStamped formation_pose_local = globalToLocalFrame(formation_pose_global);
-        // send target point and do whatever else
-        while(ros::ok())
+        // send target point and do whatever else until the drone isn't in formation
+        while(ros::ok() && !inPosition(formation_pose_local))
         {
             local_pos_pub.publish(formation_pose_local);
             ros::spinOnce();
             rate.sleep();
         }
+
+        // agent with ID = waypoint number % NUM_DRONES initiates token ring algorithm
+        // other agents carry forward token ring
+
+        continueTokenRing(local_pos_pub, formation_pose_local, drone_id, rate, token_ring_pub);
+
+        waypoint_num++;
     }
     
     return 0;
